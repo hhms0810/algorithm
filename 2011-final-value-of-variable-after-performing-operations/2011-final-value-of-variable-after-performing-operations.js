993210aa8dc50c10d5b334fd7bae8e246c2b425a
hhms0810 <140363396+hhms0810@users.noreply.github.com>
2023-08-04 21:26:43 -0700

    Time: 48 ms (96.76%), Space: 42.6 MB (42.05%) - LeetHub

--- 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.js
+++ 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.js
@@ -6,11 +6,9 @@ var finalValueAfterOperations = function(operations) {
     return operations.reduce((acc, cur) => {
         switch(cur) {
             case '--X':
-                return acc - 1;
             case 'X--':
                 return acc - 1;
             case '++X':
-                return acc + 1;
             case 'X++':
                 return acc + 1;
         }
