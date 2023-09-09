ce60b4128aec96f6ed8e74b7e71008902ce55f5f
hhms0810 <140363396+hhms0810@users.noreply.github.com>
2023-09-09 14:50:10 +0900

    Time: 51 ms (65.51%), Space: 42.1 MB (15.33%) - LeetHub

--- 2396-strictly-palindromic-number/2396-strictly-palindromic-number.js
+++ 2396-strictly-palindromic-number/2396-strictly-palindromic-number.js
@@ -5,12 +5,6 @@
 var isStrictlyPalindromic = function(n) {
     for (let i = 2; i <= n - 2; i++) {
         const base = n.toString(i);
-        
-        for (let j = 0; j < n.length; j++) {
-            if (n[j] !== 0 && n[j] !== 1) {
-                break;
-            }
-        }
         const reverseBase = base.split('').reverse().join('');
 
         if (base !== reverseBase) {
